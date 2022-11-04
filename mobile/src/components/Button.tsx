a5ae305b59d514e5fcb7a9c3551df6c2b1fdc710
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-04 11:54:44 -0300

    Button native base + pŕops

---
+++ mobile/src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { Button as ButtonNativeBase, IButtonProps, Text } from "native-base"
+
+interface ButtonProps extends IButtonProps {
+  title: string
+}
+
+export function Button({ title }: ButtonProps) {
+  return (
+    <ButtonNativeBase>
+      <Text>{title}</Text>
+    </ButtonNativeBase>
+  )
+}
