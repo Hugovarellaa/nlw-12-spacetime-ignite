4d25355329f7e1fce24ad3e6d2b83df236dbdc86
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-04 14:13:36 -0300

    Interface de autenticação

--- mobile/src/screens/SignIn/index.tsx
+++ mobile/src/screens/SignIn/index.tsx
@@ -1,18 +1,19 @@
 import { Fontisto } from "@expo/vector-icons"
-import { Center, Icon } from "native-base"
+import { Center, Icon, Text } from "native-base"
 import Logo from "../../assets/logo.svg"
 import { Button } from "../../components/Button"
 
 export function SignIn() {
   return (
-    <Center flex={1} bg="gray.900">
+    <Center flex={1} bg="gray.900" p={7}>
       <Logo width={212} height={40} />
       <Button
         type="SECONDARY"
-        leftIcon={<Icon as={Fontisto} name='google' color="white" size="md" />}
         title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name='google' color="white" size="md" />}
+        mt="12"
       />
-
+      <Text color="white" textAlign="center" mt={4}>Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação de sua conta.</Text>
     </Center>
   )
 }
