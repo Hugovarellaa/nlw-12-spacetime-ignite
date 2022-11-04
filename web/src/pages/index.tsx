02ed5e8145517f121141f32dcfdf9fea72faf728
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-04 19:27:18 -0300

    Acessando back end

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,15 +1,25 @@
 import { GetServerSideProps } from 'next'
 
-export default function Home() {
+interface HomeProps {
+  count: string
+}
+
+export default function Home({ count }: HomeProps) {
   return (
     <div>
       <h1>Primeiro log</h1>
+      <h1>{count}</h1>
     </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
   return {
-    props: {},
+    props: {
+      count: data.count,
+    },
   }
 }
