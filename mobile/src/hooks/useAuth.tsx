a2861528dcc52307573c695412b9a3596f0329d0
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-04 16:42:37 -0300

    Recomeçando app

--- mobile/src/hooks/useAuth.tsx
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { AuthContext, AuthContextProps } from "../context/AuthContext";
-
-export const useAuth = (): AuthContextProps => useContext(AuthContext)
--- mobile/src/screens/New.tsx
+++
@@ -1,9 +0,0 @@
-import { VStack } from "native-base";
-
-export function New() {
-  return (
-    <VStack flex={1} bg="gray.900" >
-
-    </VStack>
-  )
-}
