000616346d0059909e6c3b8b43c5a751bdda3f54
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-04 09:41:22 -0300

    Layout web

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,9 +6,63 @@ interface HomeProps {
 
 export default function Home({ count }: HomeProps) {
   return (
-    <div>
-      <h1>Primeiro log</h1>
-      <h1>{count}</h1>
+    <div className="max-w-[1124px] h-screen mx-auto grid gap-28 grid-cols-2 items-center">
+      <main>
+        <img src="/logo.svg" alt="" />
+        <h1 className="mt-16 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className="mt-10 flex items-center gap-2">
+          <img src="/avatares.png" alt="" />
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+12.592 </span>pessoas já estão
+            usando
+          </strong>
+        </div>
+
+        <form action="" className="mt-10 flex gap-2">
+          <input
+            type="text"
+            required
+            placeholder="Qual nome do seu bolão?"
+            className="flex flex-1 px-6 py-4 rounded bg-gray-800 border-gray-600 text-sm"
+          />
+          <button
+            type="submit"
+            className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm hover:bg-yellow-700"
+          >
+            CRIAR MEU BOLÃO
+          </button>
+        </form>
+
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar
+          para convidar outras pessoas 🚀
+        </p>
+
+        <div className="mt-10 pt-10 border-t border-gray-600 flex justify-between text-gray-100">
+          <div className="flex items-center gap-6">
+            <img src="/check.svg" alt="check" />
+            <div className="flex flex-col">
+              <strong className="font-bold text-2xl">+2.034</strong>
+              <span>Bolões criados </span>
+            </div>
+          </div>
+
+          <div className="w-px h-14 bg-gray-600" />
+
+          <div className="flex items-center gap-6">
+            <img src="/check.svg" alt="check" />
+            <div className="flex flex-col">
+              <strong className="font-bold text-2xl">+192.847</strong>
+              <span>Palpites enviados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <img src="/mobile.png" alt="2 celular com layout do aplicativo" />
     </div>
   )
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
+      backgroundImage: {
+        app: 'url(/background.png)',
+      },
+
+      colors: {
+        yellow: {
+          500: '#F7DD43',
+          700: '#e5cd3d',
+        },
+        ignite: {
+          500: '#129e57',
+        },
+        gray: {
+          100: '#E1E1E6',
+          300: '#8d8d99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214',
+        },
+      },
+    },
   },
   plugins: [],
 }
