1dfcebe2a07a0ff81222dcc2ef81053928430514
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-04 19:25:15 -0300

    _document + fonts

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,15 @@
+import { GetServerSideProps } from 'next'
+
 export default function Home() {
   return (
     <div>
       <h1>Primeiro log</h1>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {},
+  }
 }
