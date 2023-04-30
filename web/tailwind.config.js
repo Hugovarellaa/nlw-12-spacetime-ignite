c65d18eb4e854d8c5652d4d1dc3357b768455ee7
hugo <hugovarellaa@gmail.com>
2023-04-30 11:06:45 -0300

    chore: Add colors gray

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,11 +6,18 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      gray: {
+        50: '#eaeaea',
+        100: '#bebebf',
+        200: '#9e9ea0',
+        300: '#727275',
+        400: '#56565a',
+        500: '#2c2c31',
+        600: '#28282d',
+        700: '#1f1f23',
+        800: '#18181b',
+        900: '#121215',
       },
     },
   },
