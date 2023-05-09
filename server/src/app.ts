6ec62dbd6f4022a3111f203a822d381416b6f00b
hugo <hugovarellaa@gmail.com>
2023-05-09 19:39:20 -0300

    feat: install and config cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,12 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
 import { MemoriesRoutes } from './routes/memories.routes'
 
 export const app = fastify()
+
+app.register(cors, {
+	origin: '*',
+})
 
 app.register(MemoriesRoutes, {
 	prefix: '/memories',
