f79df70b050b22e2dfaee42080e506db088c61be
hugo <hugovarellaa@gmail.com>
2023-05-11 11:22:18 -0300

    refact: Refactoring page.tsx create copyright

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,17 @@
+export function Copyright() {
+  return (
+    <div>
+      <p className="text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da{' '}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://app.rocketseat.com.br/"
+          className="underline hover:text-gray-100"
+        >
+          Rocketseat
+        </a>
+      </p>
+    </div>
+  )
+}
