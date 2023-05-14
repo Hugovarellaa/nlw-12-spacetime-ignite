95652be0f29b50bf091e395746ea0805b94cf112
hugo <hugovarellaa@gmail.com>
2023-05-14 12:55:12 -0300

    chore: ensure authentication

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import fastify from 'fastify'
+import { env } from './env'
 import { AuthRoutes } from './routes/auth.routes'
 import { MemoriesRoutes } from './routes/memories.routes'
 
@@ -7,6 +9,10 @@ export const app = fastify()
 
 app.register(cors, {
 	origin: '*',
+})
+
+app.register(jwt, {
+	secret: env.SECRET,
 })
 
 app.register(AuthRoutes)
