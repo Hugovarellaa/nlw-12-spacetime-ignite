6eecc3272a3f2cf94b9065fb9b7998fc9baa1700
hugo <hugovarellaa@gmail.com>
2023-05-15 13:22:48 -0300

    feat: save jwt in cookies to user

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,21 @@
+import { api } from '@/lib/api'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url!)
+  const code = searchParams.get('code')
+
+  const sessionResponse = await api.post('/sessions', {
+    code,
+  })
+
+  const { token } = sessionResponse.data
+  const redirectUrl = new URL('/', request.url)
+  const cookieExpiresInSecond = 60 * 60 * 24 * 30 // 30 days
+
+  return NextResponse.redirect(redirectUrl, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSecond}`,
+    },
+  })
+}
